d16aba1632bd9e485cf4f3ea42c13d1f80172f70
victoritis <v.gdelcampo@udc.es>
2023-03-02 14:17:20 +0100

    Practica 1 A)

--- P1/Practica_1/ES/GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Busqueda_Anchura.java
+++ P1/Practica_1/ES/GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Busqueda_Anchura.java
@@ -3,6 +3,7 @@ package es.udc.sistemasinteligentes.ejemplo;
 import java.util.LinkedList;
 import java.util.Queue;
 
+//IMPLEMENTACION DE BUSQUEDA ANCHURA, SE REDEFINE SOLAMENTE EL ARREGLO DE DATOS DONDE SE GURDAN LOS NODOS
 public class Busqueda_Anchura extends Estrategia_BusquedaGrafo{
 
     @Override
--- P1/Practica_1/ES/GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Busqueda_Profundidad.java
+++ P1/Practica_1/ES/GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Busqueda_Profundidad.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Collections;
 
+//IMPLEMENTACION DE BUSQUEDA PROFUNDIDAD, SE REDEFINE SOLAMENTE EL ARREGLO DE DATOS DONDE SE GURDAN LOS NODOS
 public class Busqueda_Profundidad extends Estrategia_BusquedaGrafo {
     @Override
     protected Queue<Nodo> getEstructuraFrontera(){
